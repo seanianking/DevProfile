bdf10e4a7ddf758d88fc3d53315b478238e1805b
seanianking <seanianking@gmail.com>
2020-03-28 16:48:30 -0600

    created file structure, commented needed js functions.

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+//This assignment will require functions to run the inquire protocol
+//for user input, and grab from the github API (using axios), to get  
+//the Username, Color, and Star information.
+//Functions to compile the information, generate HTML, and create and save
+//the HTML as a PDF.
+
+
+
+
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+
+init();
