2e9a8d458c85f77fc36d4ccb98baba4a03e384a7
seanianking <seanianking@gmail.com>
2020-03-28 16:55:57 -0600

    added const for all required dependencies and installed them.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,18 @@
 //This assignment will require functions to run the inquire protocol
 //for user input, and grab from the github API (using axios), to get  
 //the Username, Color, and Star information.
-//Functions to compile the information, generate HTML, and create and save
-//the HTML as a PDF.
+//Functions to compile the information, generate HTML, and create and
+//save the HTML as a PDF.
+//-------------------------------------------------------------------//
 
-
-
+//**Required Dependencies **/
+const inquirer = require("inquirer");
+const generateHTML = require("./generate");
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
+const puppeteer = require("puppeteer");
+const asyncProcess = util.promisify(fs.writeFile);
 
 const questions = [
   
