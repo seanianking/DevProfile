4f80c9743b073fc68336d8179720477d9fc5ca0a
seanianking <seanianking@gmail.com>
2020-03-28 20:51:05 -0600

    added inquirer prompts and axios calls.

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,16 +7,34 @@
 
 //**Required Dependencies **/
 const inquirer = require("inquirer");
-const generateHTML = require("./generate");
+const generateHTML = require("./generateHTML");
 const fs = require("fs");
 const util = require("util");
 const axios = require("axios");
 const puppeteer = require("puppeteer");
 const asyncProcess = util.promisify(fs.writeFile);
 
-const questions = [
-  
-];
+//Inquirer questions:
+function getDataInput(){
+inquirer.prompt([{
+    type: "input",
+    name: "username",
+    message: "GitHub username please:"
+}, {
+    type: "list",
+    message: "Pick a color, ANY color out of these four:",
+    name: "color",
+    choices: ["green", "blue", "red", "pink"]
+}])
+return color, username;
+};
+
+function getAxiosData(username){
+    const profile = axios.get(`https://api.github.com/users/${username}`);
+    const starCount = axios.get(`https://api.github.com/users/${username}/starred`);
+    return profile, starCount;
+};
+
 
 function writeToFile(fileName, data) {
  
