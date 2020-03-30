089bb9e91513f9abe27e0629b127f78814830739
seanianking <seanianking@gmail.com>
2020-03-29 20:43:04 -0600

    split axios and inquirer functions, updated variables in init function. PDF is generated when terminal is run. name and employer not displaying correctly.

--- js/index.js
+++ js/index.js
@@ -17,72 +17,108 @@ const asyncWriter = util.promisify(fs.writeFile);
 // function init();
 
 //Inquirer questions:
-function getDataInput(){
-inquirer.prompt([{
+// function getDataInput(){
+// inquirer.prompt([{
+//     type: "input",
+//     name: "username",
+//     message: "GitHub username please:"
+// }, 
+
+// {
+//     type: "list",
+//     message: "Pick a color, ANY color... out of these four:",
+//     name: "color",
+//     choices: ["green", "blue", "red", "pink"]
+
+// }])
+
+// return username, color;
+// };
+
+//Previous function split into two:
+function getUsername() {
+  let username = inquirer.prompt({
     type: "input",
     name: "username",
     message: "GitHub username please:"
-}, 
+  })
+  return username
+};
 
-{
+function getColor() {
+  let color = inquirer.prompt({
     type: "list",
     message: "Pick a color, ANY color... out of these four:",
     name: "color",
     choices: ["green", "blue", "red", "pink"]
-
-}])
-
-return username, color;
+  })
+  return color
 };
 
+
+
+//Previous function (getAxiosData) split into two functions:
 //Pull information with GitHub's API via axios call.
-function getAxiosData(username){
-    const profile = axios.get(`https://api.github.com/users/${username}`);
-    const getStar = axios.get(`https://api.github.com/users/${username}/starred`);
-    return profile, getStar;
+function getAxiosProfile(username) {
+  const profile = axios.get(`https://api.github.com/users/${username}`);
+  // const getStar = axios.get(`https://api.github.com/users/${username}/starred`);
+  return profile;
 };
 
-async function init(){
-    try {
-        let {profile} = await getAxiosData();
-        const {color} = await getDataInput();
-        let getStar = await getAxiosData(profile);
-        let {data} = await getAxiosData(profile);
-        let starCount = getStar.data.length
-        data.color = color;
-        data.starCount = starCount;
+function getAxiosStar(username) {
+  // const profile = axios.get(`https://api.github.com/users/${username}`);
+  const getStar = axios.get(`https://api.github.com/users/${username}/starred`);
+  return getStar;
+};
 
-        const generator = createHTML(profile);
-        asyncWriter("first.html", generator).then(function(){
-            console.log("Congrats, it's an HTML!");
-        });
-        //call function that gets the pdf
-        getPDF();
-    } catch (err) {
-            console.log(err);
-    } 
+async function init() {
+  try {
+    let { username } = await getUsername();
+    let { color } = await getColor();
+    
+    let profile = await getAxiosProfile(username);
+    // const {color} = await getDataInput();
+    let getStar = await getAxiosStar(username);
+    let starNumber = getStar.data.length;
+    profile.data.starCount = starNumber;
+    profile.data.color = color;
+    // let {data} = await getAxiosData(profile);
+    // let starCount = getStar.data.length
+    // data.color = color;
+    // data.starCount = starCount;
+
+    const generator = createHTML(profile.data);
+    asyncWriter(`${username}.html`, generator).then(function(){
+        console.log("Congrats, it's an HTML!");
+    });
+    // call function that gets the pdf
+    getPDF(generator, username);
+
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 async function getPDF() {
-    try {
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      await page.goto(
-        "file:" + __dirname,
-        console.log(__dirname), {waitUntil: "networkidle2"}
-      );  
-      await page.pdf({
-        path: `${profile}.pdf`,
-        pageRanges: "1",
-        format: "A4",
-        printBackground: true
-      });
-      await browser.close();
-      console.log(`PDF Created at ${profile}.pdf`);
-    } catch (err) {
-      console.log(err);
-    }
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(
+      "file:" + __dirname,
+      console.log(__dirname), { waitUntil: "networkidle2" }
+    );
+    await page.pdf({
+      path: `${profile}.pdf`,
+      pageRanges: "1",
+      format: "A4",
+      printBackground: true
+    });
+    await browser.close();
+    console.log(`PDF Created at ${profile}.pdf`);
+  } catch (err) {
+    console.log(err);
   }
+}
 
 init();
 
@@ -90,7 +126,7 @@ init();
 
 
 // function writeToFile(fileName, data) {
- 
+
 // }
 
 // function init() {
